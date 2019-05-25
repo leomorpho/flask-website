23c6890b895f54416a6a87b67559d3a00671b582
Léo Audibert <leonard.audibert@pm.me>
2019-05-25 14:01:32 -0700

    Miguel Grinberg Tutorial part 5: User Logins. Users can now register and login.

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from  flask_migrate import Migrate
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+# login_view is used by LoginManager for pages that require
+# that user be logged-in.
+login.login_view = 'login'
 
 from myapp import routes, models
