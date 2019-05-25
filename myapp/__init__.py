f67072c2fec8a932ec505dbd8387fce4b751fe8b
Léo Audibert <leonard.audibert@pm.me>
2019-05-24 17:17:16 -0700

    Back to a minimum stripped down version, but it is working! The only output is hello world. Let's go from a blank slate. I deleted all past pip packages, in the hopes that flask_login will work normally now.

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,24 +1,5 @@
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
-from flask import Flask, request, current_app
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-from config import Config
+from flask import Flask
 
-db = SQLAlchemy()
-migrate= Migrate()
-login = LoginManager()
+app = Flask(__name__)
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login.init_app(app)
-
-    return app
-
-# from myapp import models
+from myapp import routes
--- website.py
+++ website.py
@@ -1,8 +1,1 @@
-from myapp import create_app, db
-# from myapp.models import User, Post
-
-app = create_app()
-
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {'db': db, 'User': User, 'Post': Post}
+from myapp import app
