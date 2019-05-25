cdcbd82ed0d226d6053269187e107c21c2a746f1
Léo Audibert <leonard.audibert@pm.me>
2019-05-25 09:52:45 -0700

    "App is working in current state. Completed the Miguel Grinberg tutorial to part 3, which is called 'Web Forms'. "

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from myapp import app
 from myapp.forms import LoginForm
 
@@ -18,8 +18,39 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/catalogue')
+def catalogue():
+    return render_template('index.html', title='Le Catalogue')
+
+@app.route('/boulangerie')
+def boulangerie():
+    return render_template('index.html', title='Boulangerie')
+
+@app.route('/patisserie')
+def patisserie():
+    return render_template('index.html', title='Patisserie')
+
+@app.route('/bistro')
+def bistro():
+    return render_template('index.html', title='Bistro')
+
+@app.route('/about')
+def about():
+    return render_template('index.html', title='About')
+
+@app.route('/contactUs')
+def contact_us():
+    return render_template('index.html', title='Contact Us')
+
+@app.route('/blog')
+def le_blog():
+    return render_template('index.html', title='Le Blog')
