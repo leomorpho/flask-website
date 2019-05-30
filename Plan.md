c3afd4389162e8b486431440dc8684cb0de943f3
Léo Audibert <leonard.audibert@pm.me>
2019-05-30 10:21:33 -0700

    Miguel Grinberg tut part9, pagination, finished.

---
+++ Plan.md
@@ -0,0 +1,21 @@
+# Plan
+
+## Olivier's Breads
+* catalogue page with products
+* products sorted by families
+* Blog
+
+## CMS
+* Recommended products
+### Administration panel
+* CRUD products with images and ingredients/tags
+* CRUD users with tiered permissions
+    * admin, editors, authors, contributors
+    * custom permissions
+* CRUD blog
+* Post to instagram and facebook with set times and queue
+* Analytics (javascript)
+* Versioning for quick rollback
+* Search feature (ElasticSearch)
+* Robust templating
+* Multiple themes
--- myapp/forms.py
+++ myapp/forms.py
@@ -45,6 +45,13 @@ class EditProfileForm(FlaskForm):
 
     def validate_username(self, username):
         if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
+            user = User.query.filter_by(
+                    username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
