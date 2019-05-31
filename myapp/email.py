e61773c2d2f19442ac9d8934fd051e2e77da68a6
Léo Audibert <leonard.audibert@pm.me>
2019-05-31 08:39:16 -0700

    Auth and Error blueprints are done and working. Cleaned up the code to reflect that (each code exists in only one place, whereas on the last commit, it was implemented correctly, but the all the original routes were still in the original file)

--- myapp/email.py
+++ myapp/email.py
@@ -29,18 +29,3 @@ def send_email(subject, sender, recipients, text_body, html_body):
     # In this case here, flask-mail need to access config settings
     # for the email server stored in app.config.
     Thread(target=send_async_email, args=(app, msg)).start()
-
-
-def send_password_reset_email(user):
-    token = user.get_reset_password_token()
-    app_name = app.config['APP_NAME']
-    subject = "[%s] Reset your password" % (app.config['APP_NAME'])
-    send_email(subject,
-               sender=app.config['ADMINS'][0],
-               recipients=[user.email],
-               text_body=render_template(
-                   'email/reset_password.txt', user=user,
-                   token=token, app_name=app_name),
-               html_body=render_template(
-                   'email/reset_password.html', user=user,
-                   token=token, app_name=app_name))
