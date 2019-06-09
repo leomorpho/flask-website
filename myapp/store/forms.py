a2072b30d7241ed09d531a77ff8fe07095d2552b
Léo Audibert <leonard.audibert@pm.me>
2019-06-09 11:13:15 -0700

    product can be added with a category

--- myapp/store/forms.py
+++ myapp/store/forms.py
@@ -1,19 +1,56 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import ValidationError, DataRequired, \
-    Email, EqualTo, Length
+from wtforms import StringField, BooleanField, SubmitField, \
+    IntegerField, SelectField
+from wtforms.validators import ValidationError, DataRequired
+from myapp.models import Product
 
 
-class CreateNewProductForm(FlaskForm):
+class ProductForm(FlaskForm):
     name = StringField('Product Name', validators=[DataRequired()])
     description = StringField('Product Description',
                               validators=[DataRequired()])
     # image = check file upload for flask-form
-    # category = StringField('Category',
-    #                       validators=[DataRequired()])
+    category = SelectField('Category', coerce=int)
+    weight = IntegerField('Weight (grams)', validators=[DataRequired()])
+
+    def validate_name(self, name):
+        product = Product.query.filter_by(name=name.data).first()
+        if product is not None:
+            raise ValidationError('Product name already in use. \
+                    Please use a different one.')
+
+
+class CreateNewProductForm(FlaskForm):
+    #    product = FormField(ProductForm)
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = StringField('Product Description',
+                              validators=[DataRequired()])
+    # image = check file upload for flask-form
+    category = SelectField('Category', coerce=int)
     weight = IntegerField('Weight (grams)', validators=[DataRequired()])
     submit = SubmitField('Create Product')
 
+    def validate_name(self, name):
+        product = Product.query.filter_by(name=name.data).first()
+        if product is not None:
+            raise ValidationError('Product name already in use. \
+                    Please use a different one.')
+
+
+class EditProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = StringField('Product Description',
+                              validators=[DataRequired()])
+    # image = check file upload for flask-form
+    category = SelectField('Category')
+    weight = IntegerField('Weight (grams)', validators=[DataRequired()])
+    submit = SubmitField('Create Product')
+
+    def validate_name(self, name):
+        product = Product.query.filter_by(name=name.data).first()
+        if product is not None:
+            raise ValidationError('Product name already in use. \
+                    Please use a different one.')
 #     def validate_username(self, username):
 #         user = User.query.filter_by(username=username.data).first()
 #         if user is not None:
--- myapp/store/routes.py
+++ myapp/store/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required
 from myapp.models import Product, ProductCategory
 from myapp.store import bp
-from myapp.store.forms import CreateNewProductForm
+from myapp.store.forms import CreateNewProductForm, \
+        EditProductForm
 from myapp import db
 
 
@@ -17,11 +18,16 @@ def store():
 @bp.route('/create_product', methods=['GET', 'POST'])
 @login_required
 def create_product():
+    categories = ProductCategory.query.order_by('name')
     form = CreateNewProductForm()
-    categories = ProductCategory.query.all()
+    form.category.choices = [(c.id, c.name) for c in categories]
+#     if request.method == 'POST':
+#         category = ProductCategory.query.filter_by(
+#                 id=form.category.data).first()
+#         return 'Name: {}, Category: {}'.format(form.name.data, category.name)
     if form.validate_on_submit():
-        # category = ProductCategory.query.filter_by(
-        #         form.category.data)
+        category = ProductCategory.query.filter_by(
+                id=form.category.data).first()
         product = Product(
             name=form.name.data,
             description=form.description.data,
@@ -29,15 +35,34 @@ def create_product():
             # Create a method to make a thumbnail from a full image?
             # thumbnail=
             # Not sure about how to add category to a new product
-            # category=category,
+            category=category,
             weight=form.weight.data)
         db.session.add(product)
         db.session.commit()
         flash('Your product has been added!')
         return redirect(url_for('store.store'))
     return render_template('store/create_product.html',
-                           form=form, categories=categories)
+                           form=form)
 
+
+# @bp.route('/create_product', methods=['GET', 'POST'])
+# @login_required
+# def edit_product():
+#     form = EditProductForm()
+#     form.category.choices = [(c.id, c.name) for c in
+#                              ProductCategory.query.order_by('name')]
+#     if form.validate_on_submit():
+#         category = ProductCategory.query.filter_by(
+#             form.category.data)
+#         product = Product(
+#             name=form.name.data,
+#             description=form.description.data,
+#             # image=form.image.data,
+#             # Create a method to make a thumbnail from a full image?
+#             # thumbnail=
+#             # Not sure about how to add category to a new product
+#             category=category,
+#             weight=form.weight.data)
 
 # @bp.route('/store/<product_name>')
 # def store(product_name):
